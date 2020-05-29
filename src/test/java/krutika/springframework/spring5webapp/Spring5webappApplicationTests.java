3f6d355038aff79535aa778924f357b1b1223094
Krutika Patil <krutiks89@gmail.com>
2020-05-29 16:43:04 -0400

    second commit

---
+++ src/test/java/krutika/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package krutika.springframework.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
